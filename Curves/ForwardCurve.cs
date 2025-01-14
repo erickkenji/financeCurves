afa6d1e88e5ac22d52b26cfd5dbcb230dc6e0aba
Kenji <erick.kenji10@gmail.com>
2025-01-14 13:37:14 -0300

    Commented old code

--- Curves/ForwardCurve.cs
+++ Curves/ForwardCurve.cs
@@ -2,7 +2,7 @@
 
 namespace Curves
 {
-    public class ForwardCurve : ICurve
+    public class ForwardCurve
     {
         private List<INode> nodes;
 
@@ -14,44 +14,44 @@ namespace Curves
 
         private void LinkNodes()
         {
-            for (int i = 0; i < nodes.Count; i++)
-            {
-                if (i > 0)
-                {
-                    nodes[i].PreviousNode = nodes[i - 1];
-                }
-                if (i < nodes.Count - 1)
-                {
-                    nodes[i].NextNode = nodes[i + 1];
-                }
-            }
+            //for (int i = 0; i < nodes.Count; i++)
+            //{
+            //    if (i > 0)
+            //    {
+            //        nodes[i].PreviousNode = nodes[i - 1];
+            //    }
+            //    if (i < nodes.Count - 1)
+            //    {
+            //        nodes[i].NextNode = nodes[i + 1];
+            //    }
+            //}
         }
 
-        public double Rate(DateTime time)
-        {
-            // Retorna a taxa forward para o tempo fornecido
-            INode node = GetNode(time);
-            return node?.Rate ?? InterpolateRate(time);
-        }
+        //public double Rate(DateTime time)
+        //{
+        //    // Retorna a taxa forward para o tempo fornecido
+        //    INode node = GetNode(time);
+        //    return node?.Rate ?? InterpolateRate(time);
+        //}
 
-        public double Factor(DateTime time)
-        {
-            // Retorna o fator forward a partir da taxa forward
-            double timeInYears = (time - DateTime.Today).TotalDays / 365.0;
-            return Math.Exp(-Rate(time) * timeInYears);
-        }
+        //public double Factor(DateTime time)
+        //{
+        //    // Retorna o fator forward a partir da taxa forward
+        //    double timeInYears = (time - DateTime.Today).TotalDays / 365.0;
+        //    return Math.Exp(-Rate(time) * timeInYears);
+        //}
 
-        public SpotCurve ToSpotCurve()
-        {
-            List<INode> spotNodes = new List<INode>();
+        //public SpotCurve ToSpotCurve()
+        //{
+        //    List<INode> spotNodes = new List<INode>();
 
-            foreach (var node in nodes)
-            {
-                spotNodes.Add(new Node(node.Time, ConvertForwardToSpot(node)));
-            }
+            //foreach (var node in nodes)
+            //{
+            //    spotNodes.Add(new Node(node.Time, ConvertForwardToSpot(node)));
+            //}
 
-            return new SpotCurve(spotNodes);
-        }
+            //return new SpotCurve(spotNodes);
+        //}
 
         private double InterpolateRate(DateTime time)
         {
@@ -65,9 +65,9 @@ namespace Curves
             return node.Rate;
         }
 
-        public INode GetNode(DateTime time)
-        {
-            return nodes.Find(n => n.Time == time);
-        }
+        //public INode GetNode(DateTime time)
+        //{
+            //return nodes.Find(n => n.Time == time);
+        //}
     }
 }
--- Curves/SpotCurve.cs
+++ Curves/SpotCurve.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 
 namespace Curves
 {
-    public class SpotCurve : ICurve
+    public class SpotCurve
     {
         private NodeCollection nodes;
 
@@ -13,31 +13,31 @@ namespace Curves
             this.nodes = nodes;
         }
 
-        public double Rate(DateTime time)
-        {
-            // Retorna a taxa spot para o tempo fornecido
-            INode node = GetNode(time);
-            return node?.Rate ?? InterpolateRate(time);
-        }
+        //public double Rate(DateTime time)
+        //{
+            //// Retorna a taxa spot para o tempo fornecido
+            //INode node = GetNode(time);
+            //return node?.Rate ?? InterpolateRate(time);
+        //}
 
-        public double Factor(DateTime time)
-        {
-            // Retorna o fator de desconto a partir da taxa spot
-            double timeInYears = (time - DateTime.Today).TotalDays / 365.0;
-            return Math.Exp(-Rate(time) * timeInYears);
-        }
+        //public double Factor(DateTime time)
+        //{
+        //    // Retorna o fator de desconto a partir da taxa spot
+        //    double timeInYears = (time - DateTime.Today).TotalDays / 365.0;
+        //    return Math.Exp(-Rate(time) * timeInYears);
+        //}
 
-        public ForwardCurve ToForwardCurve()
-        {
-            List<INode> forwardNodes = new List<INode>();
+        //public ForwardCurve ToForwardCurve()
+        //{
+        //    List<INode> forwardNodes = new List<INode>();
 
-            foreach (var node in nodes)
-            {
-                forwardNodes.Add(new Node(node.Time, ConvertSpotToForward(node)));
-            }
+        //    foreach (var node in nodes)
+        //    {
+        //        forwardNodes.Add(new Node(node.Time, ConvertSpotToForward(node)));
+        //    }
 
-            return new ForwardCurve(forwardNodes);
-        }
+        //    return new ForwardCurve(forwardNodes);
+        //}
 
         private double InterpolateRate(DateTime time)
         {
@@ -51,30 +51,30 @@ namespace Curves
             return node.Rate;
         }
 
-        public INode GetNode(DateTime time)
-        {
-            return nodes.Find(n => n.Time == time);
-        }
+        //public INode GetNode(DateTime time)
+        //{
+        //    return nodes.Find(n => n.Time == time);
+        //}
     }
    
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            // Exemplo de criação de buckets
-            List<INode> spotBuckets = new List<INode>
-        {
-            new Bucket(new DateTime(2025, 8, 23), 0.02),
-            new Bucket(new DateTime(2026, 8, 23), 0.025),
-            new Bucket(new DateTime(2027, 8, 23), 0.03)
-        };
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        // Exemplo de criação de buckets
+    //        List<INode> spotBuckets = new List<INode>
+    //    {
+    //        new Bucket(new DateTime(2025, 8, 23), 0.02),
+    //        new Bucket(new DateTime(2026, 8, 23), 0.025),
+    //        new Bucket(new DateTime(2027, 8, 23), 0.03)
+    //    };
 
-            SpotCurve spotCurve = new SpotCurve(spotBuckets);
-            ForwardCurve forwardCurve = spotCurve.ToForwardCurve();
+    //        SpotCurve spotCurve = new SpotCurve(spotBuckets);
+    //        ForwardCurve forwardCurve = spotCurve.ToForwardCurve();
 
-            DateTime testDate = new DateTime(2026, 8, 23);
-            Console.WriteLine($"Spot Rate for {testDate.ToShortDateString()}: {spotCurve.Rate(testDate)}");
-            Console.WriteLine($"Forward Rate for {testDate.ToShortDateString()}: {forwardCurve.Rate(testDate)}");
-        }
-    }
+    //        DateTime testDate = new DateTime(2026, 8, 23);
+    //        Console.WriteLine($"Spot Rate for {testDate.ToShortDateString()}: {spotCurve.Rate(testDate)}");
+    //        Console.WriteLine($"Forward Rate for {testDate.ToShortDateString()}: {forwardCurve.Rate(testDate)}");
+    //    }
+    //}
 }
