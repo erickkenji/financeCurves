f5f6f3e0b6d8785015b1afeff2706e3b04e9da4b
Kenji <erick.kenji10@gmail.com>
2025-04-20 11:49:30 -0300

    Added standard deviation

---
+++ CalculateError/StandardDeviation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metrics
+{
+    public class StandardDeviation
+    {
+        public static double Calculate(double[] values)
+        {
+            // Calcular a média (μ) dos valores
+            double medium = values.Average();
+
+            // Calcular a soma dos quadrados das diferenças em relação à média
+            double quadraticSum = values.Select(val => Math.Pow(val - medium, 2)).Sum();
+
+            // Calcular o desvio padrão
+            double standardDeviation = Math.Sqrt(quadraticSum / values.Length);
+            return standardDeviation;
+        }
+    }
+}
